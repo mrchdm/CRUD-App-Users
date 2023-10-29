a81d0beb52266ab178f68eb14a1be6467ebcf723
mrchdm <dementevamm@yandex.ru>
2023-10-30 00:42:02 +0300

    add form

--- src/Components/UserForm.tsx
+++ src/Components/UserForm.tsx
@@ -1,70 +1,72 @@
-import React, { FC } from "react"
-import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import {SubmitHandler, useForm } from 'react-hook-form'
 import { UserTypes } from "../Types/userForm.interface"
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {Stack} from "@mui/material";
 import UserStore from "../Store/user-store";
-import UsersService from "../API/UsersService";
 import { observer } from "mobx-react-lite";
 import { useEffect } from 'react';
 
-type UserProps = {
-  users: object[]
-}
+
 
 const UserForm = observer(() => {
 
   const {
     register,
     handleSubmit,
-    watch,
+    reset,
     formState: { errors },
-    control, } = useForm<UserTypes>({})
-
+     } = useForm<UserTypes>({})
 
   const onSubmit: SubmitHandler<UserTypes> = (data) => {
-   
-    const user = {
-      name: data.name,
-      username: data.username,
-      phone: data.phone,
-      email: data.email
-    }
-    return UserStore.addUser(user)    
+    UserStore.editFormValue 
+      ? UserStore.updateUser({ oldUsername: UserStore.editFormValue.username, ...data })
+      : UserStore.addUser(data);
+    reset();
   }
+
+  useEffect(() => {
+    if (UserStore.editFormValue) {
+      reset(UserStore.editFormValue);
+    }
+  }, [UserStore.editFormValue, reset])
+
 
   return (
     <>
-    <h1>{}</h1>
-      <Typography style={{ textAlign: 'center' }} variant="h4" component="h2">Введите данные пользователя</Typography>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid
-          container
+        <Stack
           direction="column"
-          justifyContent="space-evenly"
-          alignItems="center"
+          justifyContent="center"
+          alignItems="stretch"
+          spacing={2}
+          maxWidth='800px'
+          minHeight='360px'
+          margin='20px auto'
         >
-          <TextField {...register('name', { required: 'Имя обязательно' })} type="text" placeholder='Name' id="name" label="Your name" variant="outlined" />
+          <TextField {...register('name', { required: '* Name is required' })} type="text" placeholder='Name' id="name" label=" Name" variant="outlined" />
           {errors?.name && (<div style={{ color: 'red' }}>{errors.name.message}</div>)}
 
-          <TextField {...register('username', { required: 'Имя пользователя обязательно' })} type="text" placeholder='UserName' id="username" label=" Username" variant="outlined" />
+          <TextField {...register('username', { required: '* Username is required' })} type="text" placeholder='UserName' id="username" label=" Username" variant="outlined" />
           {errors?.username && (<div style={{ color: 'red' }}>{errors.username.message}</div>)}
 
-          <TextField {...register('phone')} type="text" placeholder='phone' id="phone" label=" phone" variant="outlined" />
+          <TextField {...register('phone')} type="text" placeholder='phone' id="phone" label="Phone" variant="outlined" />
           {errors?.phone && (<div style={{ color: 'red' }}>{errors.phone.message}</div>)}
 
           <TextField {...register('email', {
             pattern: {
               value: /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/,
-              message: 'Пожалуйста введите валидный эмайл'
+              message: '*Enter valid email'
             }
-          })} type="text" placeholder='Email' variant="outlined" label="email" />
+          })} type="text" placeholder='Email' variant="outlined" label="Email" />
           {errors?.email && (<div style={{ color: 'red' }}>{errors.email.message}</div>)}
 
-          <Button variant="contained" type="submit">Send</Button>
-        </Grid>
+          <Button variant="contained" type="submit">
+          { UserStore.editFormValue ? 'Update' : 'Send' }
+          </Button>
+        </Stack>
       </form>
+
     </>
   )
 })
